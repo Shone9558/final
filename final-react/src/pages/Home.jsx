11733775d7dae63e903bb9d7d5477846c8a0f5b4
silvercow02 <66425747+silvercow002@users.noreply.github.com>
2025-12-22 10:07:54 +0800

    .

--- final-react/src/pages/Home.jsx
+++ final-react/src/pages/Home.jsx
@@ -3,11 +3,25 @@ import EmotionButtons from "../components/EmotionButtons";
 import PlayerSection from "../components/PlayerSection";
 import Playlist from "../components/Playlist";
 import MiniPlayer from "../components/MiniPlayer";
+import { useNavigate } from "react-router-dom";
 
 export default function Home() {
+  const navigate = useNavigate();
+
+  function handleLogout() {
+    localStorage.removeItem("token");
+    navigate("/login", { replace: true });
+  }
+
   return (
     <>
-      <Header />
+      <div style={{display:"flex",justifyContent:"space-between",alignItems:"center"}}>
+        <Header />
+        <div style={{marginRight:16}}>
+          <button onClick={handleLogout}>登出</button>
+        </div>
+      </div>
+
       <EmotionButtons />
       <PlayerSection />
       <Playlist />
